eedb4e2607fca5c093502c6bc3b82a1721bef556
xrwang <xwang@gsd.harvard.edu>
2015-10-04 21:33:09 -0700

    Use ENV vars for omniauth in production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,12 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret,
-           :scope => 'email,public_profile', :image_size => 'large', :display => 'popup'
+  if Rails.env.development?
+    provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret,
+             :scope => 'email,public_profile', :image_size => 'large', :display => 'popup'
+  else
+    provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
+             :scope => 'email,public_profile', :image_size => 'large', :display => 'popup'
+  end
+
 end
